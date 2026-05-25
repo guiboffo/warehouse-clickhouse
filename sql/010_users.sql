5de32f99dab865880efce3251b822a6098f85f26
guiboffo <54599091+guiboffo@users.noreply.github.com>
2026-05-25 12:07:49 -0300

    fix: update users DDL to ReplacingMergeTree(login_at)

--- sql/010_users.sql
+++ sql/010_users.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS warehouse.users
 (
-  id Int64,
-  uuid String,
-  email String,
-  lms_user_id String,
-  organization_id UInt64,
-  login_at DateTime,
-  role String
+    id              Int64,
+    uuid            String,
+    email           String,
+    lms_user_id     String,
+    organization_id UInt64,
+    login_at        DateTime,
+    role            String
 )
-ENGINE = MergeTree
-ORDER BY (organization_id, login_at, id);
+ENGINE = ReplacingMergeTree(login_at)
+ORDER BY (organization_id, id);
